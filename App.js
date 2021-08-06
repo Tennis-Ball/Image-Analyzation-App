b233f57ff7534063dff2eaf0c2949d4ea234f4c5
unknown <choi.mason@gmail.com>
2021-08-06 11:20:26 -0400

    camera and navigation set up

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react'
+import {Text, View} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './app/screens/HomeScreen';
+import NavigationScreen from './app/screens/NavigationScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <HomeScreen/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{header: () => null}} />
+        <Stack.Screen name="Navigation" component={NavigationScreen} options={{headerTitle: 'Back'}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
